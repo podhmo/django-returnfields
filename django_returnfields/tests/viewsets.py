a954139f62ff8b1a4124dee3c94086cff8c0ff14
podhmo <podhmo+altair@beproud.jp>
2016-04-29 20:35:11 +0900

    Revert "[skip ci] see settings"
    This reverts commit 7144a37f9aa0e00660fc644f93d8068825cb2642.

--- django_returnfields/tests/viewsets.py
+++ django_returnfields/tests/viewsets.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from django_returnfields import serializer_factory
+from django_returnfields import serializer_factory, Restriction
 
 from . import serializers
 from .models import Skill
@@ -11,6 +11,13 @@ from .models import Skill
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializer_factory(serializers.UserSerializer)
+
+
+class UserViewSet2(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializer_factory(
+        serializers.UserSerializer,
+        restriction=Restriction(include_key="include", exclude_key="exclude"))
 
 
 class SkillUserViewSet(viewsets.ModelViewSet):
