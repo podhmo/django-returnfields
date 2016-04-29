a2c33ef5ec4446f4648d5881ab4a4f3f02dc3941
podhmo <podhmo+altair@beproud.jp>
2016-04-29 12:54:19 +0900

    maintenance

--- django_returnfields/__init__.py
+++ django_returnfields/__init__.py
@@ -1,30 +1,43 @@
 # -*- coding:utf-8 -*-
 from collections import OrderedDict
 
-
 INCLUDE_KEY = "return_fields"
+EXCLUDE_KEY = "exclude"
 PATH_KEY = "_drf__path"  # {name: string, candidates: string[]}[]
 
 
 class Restriction(object):
-    def __init__(self, include_key=INCLUDE_KEY, path_key=PATH_KEY):
+    def __init__(self, include_key=INCLUDE_KEY, exclude_key=EXCLUDE_KEY, path_key=PATH_KEY):
         self.include_key = include_key
+        self.exclude_key = exclude_key
         self.path_key = path_key
 
     def setup(self, serializer):
         if PATH_KEY not in serializer.context:
-            fields_names_string = self.get_params(serializer)[self.include_key]
-            includes = [field_name.strip() for field_name in fields_names_string.split(",")]
-            serializer.context[PATH_KEY] = [{"name": "", "candidates": includes}]
+            frame = {"name": "", "candidates": self.parse_passed_values(serializer)}
+            serializer.context[PATH_KEY] = [frame]
 
-    def get_params(self, serializer):
+    def get_passed_values(self, serializer):
         return serializer.context["request"].GET
 
     def is_active(self, serializer):
-        return self.include_key in self.get_params(serializer)
+        return self.include_key in self.get_passed_values(serializer)
 
-    def restrict(self, serializer, fields):
-        candidates = serializer.context[self.path_key][-1]["candidates"]
+    def parse_passed_values(self, serializer):
+        fields_names_string = self.get_passed_values(serializer)[self.include_key]
+        return [field_name.strip() for field_name in fields_names_string.split(",")]
+
+    def current_frame(self, serializer):
+        return serializer.context[self.path_key][-1]
+
+    def push_frame(self, serializer, frame):
+        return serializer.context[self.path_key].append(frame)
+
+    def pop_frame(self, serializer):
+        return serializer.context[self.path_key].pop()
+
+    def to_restricted_fields(self, serializer, fields):
+        candidates = self.current_frame(serializer)["candidates"]
         if "" in candidates:  # all fields
             return fields
 
@@ -38,16 +51,16 @@ class Restriction(object):
     def to_representation(self, serializer, data):
         if not serializer.field_name or self.path_key not in serializer.context:
             return serializer._to_representation(data)
-        frame = serializer.context[self.path_key][-1]
+        frame = self.current_frame(serializer)
         prefix = "{}__".format(serializer.field_name)
         candidates = [s.lstrip(prefix) for s in frame["candidates"]]
-        serializer.context[self.path_key].append({"name": serializer.field_name, "candidates": candidates})
+        self.push_frame(serializer, {"name": serializer.field_name, "candidates": candidates})
         ret = serializer._to_representation(data)
-        serializer.context[self.path_key].pop()
+        self.pop_frame(serializer)
         return ret
 
     def __hash__(self):
-        return hash((self.__class__, self.include_key, self.path_key))
+        return hash((self.__class__, self.include_key, self.path_key, self.exclude_key))
 
 _cache = {}
 
@@ -73,7 +86,7 @@ def serializer_factory(serializer_class, restriction=Restriction(INCLUDE_KEY, PA
         def get_restricted_fields(self, fields):
             if restriction.is_active(self):
                 restriction.setup(self)
-                return restriction.restrict(self, fields)
+                return restriction.to_restricted_fields(self, fields)
             else:
                 return fields
 
