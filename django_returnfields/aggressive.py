f9269b2f4f33cca08a37b5447a57fddca7f13f12
podhmo <podhmo+altair@beproud.jp>
2016-05-17 08:55:05 +0900

    1.7..

--- django_returnfields/aggressive.py
+++ django_returnfields/aggressive.py
@@ -48,12 +48,24 @@ class CorrectNameCollector(object):
 def new_state(s, candidates):
     return State(field=s.field, is_relation=s.is_relation, candidates=candidates)
 
-if django.VERSION < (1, 8):
-    def get_all_fields(m):
-        return m._meta.fields
-else:
+
+if django.VERSION >= (1, 8):
     def get_all_fields(m):
         return m._meta.get_fields()
+else:
+    from django.db.models.fields.related import RelatedField
+
+    class FieldAdapter(object):
+        def __init__(self, f):
+            self.f = f
+            self.is_relation = isinstance(f, RelatedField)
+
+        def __getattr__(self, k):
+            return getattr(self.f, k)
+
+    def get_all_fields(m):
+        xs = [m._meta._name_map.get(name, None)[0] for name in m._meta.get_all_field_names()]
+        return [FieldAdapter(x) for x in xs if x]
 
 
 def extract_candidates(model):
