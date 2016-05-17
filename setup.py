5db7a909d9d587ac34b3ee4f3c18f76394b70e49
podhmo <podhmo+altair@beproud.jp>
2016-05-17 09:09:00 +0900

    0.2.0

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class MyTest(TestCommand):
 
 
 setup(name='django-returnfields',
-      version='0.1.1',
+      version='0.2.0',
       description='adding restriction of your api\'s return fields, in restframework',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
